8c45c7151265ebc710f2a3b788d1200a0a2cb34a
Rippedz <ttfoot12@gmail.com>
2024-09-25 00:56:37 +0200

    typeS

---
+++ TypeScript/task_0/js/main.ts
@@ -0,0 +1,43 @@
+interface Student {
+    firstName: string;
+    lastName: string;
+    age: number;
+    location: string;
+}
+
+const student1: Student = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 20,
+    location: 'New York'
+};
+
+const student2: Student = {
+    firstName: 'Jane',
+    lastName: 'Doe',
+    age: 19,
+    location: 'California'
+};
+
+const studentsList: Array<Student> = [student1, student2];
+
+// Création et affichage du tableau HTML
+const table = document.createElement("table");
+const tbody = document.createElement("tbody");
+
+studentsList.forEach((student) => {
+  const row = document.createElement("tr");
+
+  const firstNameCell = document.createElement("td");
+  firstNameCell.textContent = student.firstName;
+
+  const locationCell = document.createElement("td");
+  locationCell.textContent = student.location;
+
+  row.appendChild(firstNameCell);
+  row.appendChild(locationCell);
+  tbody.appendChild(row);
+});
+
+table.appendChild(tbody);
+document.body.appendChild(table);
